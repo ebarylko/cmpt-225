615cc3c7f18c7b20b691e05e3d4a8f72d4850575
EItan Barylko <eitan@barylko.com>
2023-10-18 16:49:19 -0700

    Added == operator overrload for messages

--- assignment-3/a3.cpp
+++ assignment-3/a3.cpp
@@ -27,249 +27,231 @@
 //
 // Do not #include any other files!
 //
-#include "Announcement.h"
-#include "JingleNet_announcer.h"
-#include "Queue_base.h"
+#include <cassert>
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <vector>
-#include <map>
-#include <sstream>
+
+#include "Announcement.h"
+#include "JingleNet_announcer.h"
+#include "Queue_base.h"
 
 using namespace std;
 
 
-// Dos tipos de insrucciones. 
+struct InProgress {
+    string found;
+    string rest;
+};    
+
+InProgress read_word(string search_in) {
+    int space_pos = search_in.find_first_of(" ");
+    InProgress res;
+    
+    if (space_pos != -1) {
+    res.found = search_in.substr(0, space_pos);
+    res.rest = search_in.substr(space_pos + 1);
+    }
+
+    return res;
+}
+
+// Dos tipos de insrucciones.
 // una que modifica la lista entera
 // y la otra que solo modifica un queue
 
-template <typename T> class Queue : public Queue_base<T> {
+template <typename T>
+class Queue : public Queue_base<T> {
+  struct Node {
+    Node* prev;
+    T curr;
+    Node* next;
 
-    struct Node {
-       Node* prev;
-       T curr;
-       Node* next;
-    };
+    Node(T curr) : curr(curr){};
+  };
 
-    Node* first;
-    Node* last;
-    int elems;
+  Node* first;
+  Node* last;
+  int elems;
 
-    public :
-
-    Queue<T>(): first(nullptr), last(nullptr), elems(0) {};
-    ~Queue<T>() {
-        while (!this->is_empty()) {
-            this->dequeue();
-        }
-    };
-
-    int size() const {
-        return this->elems;
+ public:
+  Queue<T>() : first(nullptr), last(nullptr), elems(0){};
+  ~Queue<T>() {
+    while (!this->is_empty()) {
+      this->dequeue();
     }
+  };
 
-    bool is_empty() {
-        return !this->size();
+  int size() const { return this->elems; }
+
+  bool is_empty() const {
+    cout << !this->size() << " empty? " << this->size() << endl;
+    return !this->size();
+  }
+
+  void enqueue(const T& item) {
+    Node* new_item = new Node(item);
+    if (this->is_empty()) {
+      cout << "empty branch" << endl;
+      this->first = new_item;
+      this->last = new_item;
+      cout << "address of first " << new_item << endl;
+      cout << "curr size " << elems << endl;
+    } else {
+      cout << "non-empty branch" << endl;
+      this->last->next = new_item;
+      new_item->prev = this->last;
+      this->last = new_item;
     }
+    this->elems++;
+    cout << "curr size at end " << elems << endl;
+  }
 
-    void enqueue(const T& item) {
-       Node* new_item = new Node(nullptr, item, nullptr);
-       if (this->empty()) {
-         first = new_item;
-         last = new_item;
-         elems++;
-       } else {
-         this->last->next = new_item;
-         new_item->prev = this->last;
-         this->last = new_item;
-       }
+  void dequeue() {
+    if (this->is_empty()) {
+      throw runtime_error("dequeue: queue is empty");
     }
+    Node* remove = this->first;
+    this->first = this->first->next;
+    this->elems--;
+    this->last = this->is_empty() ? nullptr : this->last;
+    delete remove;
+  }
 
-    void dequeue() {
-        if (this->is_empty()) {
-            throw runtime_error("dequeue: queue is empty");
-        }
-        Node* remove = this->first;
-        this->first = this->first->next;
-        this->elems--;
-        this->last = this->is_empty() ? nullptr : this->last;
-        delete remove;
+  vector<T> print_items() {
+    stringstream items;
+    vector<T> itms;
+    Node* curr_itm = this->first;
+    // cout << "Size" << curr_itm << endl;
+    while (curr_itm) {
+      cout << curr_itm->curr << endl;
+      itms.push_back(curr_itm->curr);
+    //   items << curr_itm->curr;
+      curr_itm = curr_itm->next;
     }
+    return itms;
+    // return items.str();
+  };
 
-    stringstream& print_items() {
-        stringstream items;
-        Node* curr_itm = this->first;
-        while (curr_itm) {
-            items << curr_itm->curr;
-            curr_itm = curr_itm->next;
-        }
-        return items
-
-    };
-
-    const T& front() const {
-        if (this->is_empty()) {
-            throw runtime_error("front: queue is empty");
-        }
-        return this->first->curr;
+  const T& front() const {
+    if (this->is_empty()) {
+      throw runtime_error("front: queue is empty");
     }
-};      
-
-// quiero esconder el struct, before el operador de << tiene que esta afuera
-struct Message {
-    typedef string Body, Sender, Receiver;
-    Sender sender;
-    Receiver recipient;
-    Body content;
-    friend ostream& operator<<(ostream& os, Message& msg);
+    return this->first->curr;
+  }
 };
 
-ostream& operator <<(ostream& os, Message& msg) {
-    os << "[" << msg.sender << ' ';
-    os << msg.recipient << ' ';
-    os << msg.content << "] ";
-    return os;
+struct Message {
+  typedef string Body, Sender, Receiver;
+  Sender sender;
+  Body content;
+  Message(const Sender& from,const Body& content): sender(from), content(content){};
+};
+
+ostream& operator<<(ostream& os, const Message& msg) {
+  os << "[" << msg.sender << ' ';
+  os << msg.content << "] ";
+  return os;
 }
 
+bool operator==(const Message& one, const Message two) {
+    return one.content == two.content && one.sender == two.sender;
+}
 
+enum Receiver { Santa, Reindeer, Elf2, Elf1, Snowman };
+// preguntar professor sobre testing
 
-// JingleNet has 
+// JingleNet has
 // a queue for each of the following:
 // santa, reindeer, elf2, elf1, snowman
 class JingleNet {
-    enum receiver { Santa, Reindeer, Elf2, Elf1, Snowman };
 
+  // Base class for an instruction which contains a method for executing a
+  // specific instruction
+  class Instruction {
+   public:
+    virtual void execute(JingleNet system) = 0;
+    virtual ~Instruction(){};
+  };
 
-    // Base class for an instruction which contains a method for executing a
-    // specific instruction
-    class Instruction {
-        public:
-        virtual void execute(JingleNet system) = 0;
-        virtual ~Instruction();
-    };
+    Queue<Message>& get_queue(const Rank& to) {
+        return messages[(int)to - 1];
+    }
+  // Enqueues the message from the sender to the queue where the receiver is
 
-    // Enqueues the message from the sender to the queue where the receiver is
-    class Send : public Instruction {
-        Message msg;
-
-
-        public:
-
-        Send(Message new_msg): msg(new_msg){};
-
-        void set_message(Message msg) {
-            this->msg = msg;
-        }
-
-        virtual void execute(JingleNet system) {
-            // Checks where the message must be sent
-            if (msg.recipient == "santa") {
-              system.messages[Santa].enqueue(msg);
-            } else if (msg.recipient == "reindeer") {
-              system.messages[Reindeer].enqueue(msg);
-            }
-
-            else if (msg.recipient == "elf2") {
-              system.messages[Elf2].enqueue(msg);
-            }
-
-            else if (msg.recipient == "elf1") {
-              system.messages[Elf1].enqueue(msg);
-            }
-
-            else if (msg.recipient == "snowman") {
-              system.messages[Snowman].enqueue(msg);
-            }
-        }
-    };
-
-/**
- * @brief Takes the details of a message and returns a message
- * 
- * @param message_info a string with information about the sender, receiver, and message content
- * @return Message a message containing all the information in message_info
- */
-Message fill_message(char* message_info) {
-    Message new_msg;
-    string body;
-
-    // Setting the information of the message
-    message_info = strtok(NULL, " ,.-");
-    new_msg.sender = message_info;
-
-    message_info = strtok(NULL, " ,.-");
-    new_msg.recipient = message_info;
-
-    message_info = strtok(NULL, " ,.-");
-    while (message_info) {
-            body += message_info;
-            message_info = strtok(NULL, " ,.-");
+     void send_message(const Message& msg, Rank to) {
+        get_queue(to).enqueue(msg);
     }
 
-    new_msg.content = body;
-    return new_msg;
-}
+  Queue<Message> messages[5];
 
-    Queue<Message> santa_msgs, reindeer_msgs, elf1_msgs, elf2_msgs, snowman_msgs;
-    Queue<Message> messages[5] = {santa_msgs, reindeer_msgs, elf1_msgs, elf2_msgs, snowman_msgs};
 
-    public:
-/**
- * @brief Takes an instruction and applies the instruction onto the JingleNet system
- * 
- * @param instruction One of four possible commands to be executed by JingleNet
- */
-void apply_instruction(string instruction) {
-        string message;
-        char* sentence = const_cast<char*>(instruction.c_str());
-        char* word = strtok(sentence, " ");
+ public:
+  /**
+   * @brief Takes an instruction and applies the instruction onto the JingleNet
+   * system
+   *
+   * @param instruction One of four possible commands to be executed by
+   * JingleNet
+   */
+  void apply_instruction(const string& instruction) {
+    string message;
+    InProgress instr = read_word(instruction);
+    string send_instr = "SEND";
+    // Send message if the instruction was send
+    if (send_instr == instr.found) {
+      InProgress sender = read_word(instr.rest);
+      InProgress receiver = read_word(sender.rest);
+      Message msg(sender.found, receiver.rest);
+      Rank to = to_rank(receiver.found);
+      this->send_message(msg, to);
+    }
+  }
 
-        // Send message if the instruction was send
-        if (word == "SEND") {
-            Message msg = fill_message(word);
-            Send send = Send(msg);
-            send.execute(*this);
-        }
-}
-
-/**
- * @brief Takes a receiver and returns all the messages for that person
- * 
- * @param receiver one of the following targets: santa, reindeer, elf2, elf1, snowman
- * @return string all the messages the target has received
- */
-string const messages_for(string receiver) {
-    map<string, int> name_to_pos = {
-      {"santa", Santa},
-      {"reindeer", Reindeer},
-      {"elf2", Elf2},
-      {"elf1", Elf1},
-      {"snowman", Snowman}
-    };
-    int pos = name_to_pos[receiver];
-    Queue<Message> look_in = this->messages[pos];
-    stringstream& messages = look_in.print_items();
-    return messages.str();
-}
+  /**
+   * @brief Takes a receiver and returns all the messages for that person
+   *
+   * @param receiver one of the following targets: santa, reindeer, elf2, elf1,
+   * snowman
+   * @return string all the messages the target has received
+   */
+  vector<Message> const messages_for(const Rank& receiver) {
+    return get_queue(receiver).print_items();
+  }
 };
 
 // parsiar una instruccion:
 // ir linea por linea, y crear commandos
 // applicar commandos sobre las lineas
 
+// hacer una clase que te dice que estas testiando
 
-bool parse_and_apply_instruction_test() {
-    JingleNet system;
-    string instruction = "SEND a santa hi";
-    system.apply_instruction(instruction);
-    string expected = "a santa hi";
-    string actual = system.messages_for("santa");
+void test_case(string scenario) { cout << scenario << endl; }
+
+// vector<Message> mk_msgs(...) {
+
+// }
+
+void send_test() {
+  JingleNet system;
+
+  test_case(
+      "Adding a message for santa creates a new message in the santa queue");
+  string instruction = "SEND a santa hi";
+  system.apply_instruction(instruction);
+  vector<Message> expected;
+  Message expected_msg("a", "hi");
+  expected.push_back(expected_msg);
+  Rank target = Rank::SANTA;
+  vector<Message> actual = system.messages_for(target);
+  assert(expected == actual);
 }
 
-
-int main()
-{
-    cout << "Welcome to Assignment 3!" << endl;
+int main() {
+  send_test();
+  cout << "Welcome to Assignment 3!" << endl;
 }
