a24fd2fe7dc5b3cfe8664bc50dc7b911036d1c81
EItan Barylko <eitan@barylko.com>
2023-10-31 13:09:47 -0700

    Added test case for checking that rand_num generates the correct values in a range

--- assignment-4/tests.cpp
+++ assignment-4/tests.cpp
@@ -5,8 +5,10 @@
 #include "test.h"
 #include <cassert>
 #include <string>
+#include <algorithm>  
 
 using namespace std;
+
 
 TEST_CASE("rand_num") {
     SUBCASE("Generating a random number from the range [a, a] returns the value a") {
@@ -15,6 +17,19 @@ TEST_CASE("rand_num") {
                 int actual = rand_num(2, 2);
                 THEN("The number outputted should equal two") {
                     REQUIRE(actual == 2);
+                }
+            }
+        }
+    }
+    SUBCASE("Generating a value in the range [a, b] returns a value x which has the following properties: a <= x <= b") {
+        GIVEN( "A range of values [1, 100]" ) {
+            WHEN("Generating 100 values in this range") {
+                vector<int> actual;
+                for(int i = 0; i < 100; i++) {
+                    actual.push_back(rand_num(1, 100));
+                }
+                THEN("All the values should lie in between 1 and 100") {
+                    REQUIRE(all_of(actual.begin(), actual.end(), [](int val) {return 1 <= val && val <= 100;}));
                 }
             }
         }
